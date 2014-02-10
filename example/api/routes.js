43120cd580642c7e3cd20b3cc13886326130a87b
Pierre <pierre.besson7@gmail.com>
2014-02-10 09:06:28 +0100

    Add post to create vm.

--- example/api/routes.js
+++ example/api/routes.js
@@ -1,25 +1,35 @@
+var Hapi  = require('hapi');
 ///var Types = require('hapi').Types;
 module.exports = [{
-		method: 'GET',
-		path: '/vm',
-		config: {
-			handler: getVms
-			// query: {
-			// 	name: Types.String()
-			// }
+	method: 'GET',
+	path: '/vm',
+	config: {
+		handler: getVms
+		// query: {
+		// 	name: Types.String()
+		// }
+	}
+}, {
+	method: 'GET',
+	path: '/vm/{id}',
+	config: {
+		handler: getVm
+	}
+}, {
+	method: 'POST',
+	path: '/vm',
+	config: {
+		handler: addVm,
+		validate: {
+			payload: {
+				name: Hapi.types.String().required().min(3)
+			}
 		}
-	}, {
-		method: 'GET',
-		path: '/vm/{id}',
-		config: {
-			handler: getVm
-		}
-	},
-	//{ method: 'POST', path: '/vm', config: { handler: addProduct, payload: 'parse', schema: { name: Types.String().required().min(3) }, response: { id: Types.Number().required() } } }
-];
+	}
+}];
 
 var vms = [{
-	id: 1,
+	id: 0,
 	name: 'VM1',
 	nbCpu: 2,
 	memory: 1000,
@@ -28,7 +38,7 @@ var vms = [{
 	users: undefined,
 	osId: 3
 }, {
-	id: 2,
+	id: 1,
 	name: 'VM2',
 	nbCpu: 4,
 	memory: 2000,
@@ -48,4 +58,22 @@ function getVms(request, reply) {
 
 function getVm(request, reply) {
 	reply(vms[request.params.id]);
+}
+
+function addVm(request, reply) {
+
+	var vm = {
+		id: vms[vms.length - 1].id + 1,
+		name: request.payload.name,
+		nbCpu: 4,
+		memory: 2000,
+		diskTypeId: 1,
+		diskCapacity: 300,
+		users: undefined,
+		osId: 1
+	};
+
+	vms.push(vm);
+
+	reply(vm).code(201).header('Location', '/products/' + vm.id);
 }
