14c64f87504cf825c35b0fef8eabae24c315cf6d
Pierre <pierre.besson7@gmail.com>
2014-02-14 08:58:01 +0100

    Add doc on promisify.

--- app/models/promisify.js
+++ app/models/promisify.js
@@ -1,5 +1,6 @@
 /*global Backbone, Promise*/
 var PromiseModel = Backbone.Model.extend({
+	//Ovverride the save method on the model in order to Return a promise.
 	save: function saveModel() {
 		var model = this;
 		return new Promise(
@@ -15,14 +16,11 @@ var PromiseModel = Backbone.Model.extend({
 });
 
 var PromiseCollection = Backbone.Collection.extend({
+	//Ovverride the default collection fetch method, using and returning a promise.
 	fetch: function promiseFetchCollection() {
 		var collection = this;
 		return new Promise(function(resolve, reject) {
 			Backbone.sync('read', collection, {success: resolve, error: reject});
-			// collection.fetch({
-			// 	success: resolve,
-			// 	error: reject
-			// });
 		});
 	}
 });
