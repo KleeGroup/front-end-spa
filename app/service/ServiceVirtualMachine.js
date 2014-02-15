4bb96d1bca3f7074714f0e29e540737f8ec6c7bb
Pierre <pierre.besson7@gmail.com>
2014-02-15 23:35:49 +0100

    Add the searchVirtualMachine promise method.

--- app/service/ServiceVirtualMachine.js
+++ app/service/ServiceVirtualMachine.js
@@ -1,26 +1,46 @@
+/*Dependencies on both model and collection class for virtual machine.*/
 var VirtualMachine = require('../models/virtualMachine');
+var VirtualMachines = require('../models/virtualMachines');
+/*Load the promisify helper.*/
+var Promisify = require('../models/promisify');
+/*Error helper dependency.*/
 var ErrorHelper = require('../lib/error_helper');
+/*Instances of both model and collection of virtual machines.*/
 var virtualMachine = new VirtualMachine();
+var virtualMachines = new VirtualMachines();
+var promiseVirtualMachines = new Promisify.Collection();
+promiseVirtualMachines.url = virtualMachines.url;
+
 
 function saveVirtualMachine(model) {
 	virtualMachine.clear({
 		silent: true
 	});
 	virtualMachine.save(model.toJSON(), {
-		success: function(newModel, response) {
+		success: function successSaveVirtualMachine(newModel, response) {
 			model.trigger('save:success', newModel);
 		},
-		error: function(newModel, response) {
+		error: function errorSaveVirtualMachine(newModel, response) {
 			model.trigger('save:error', ErrorHelper.manageResponseErrors(response));
 		}
 	});
 }
 
-function deleteVm(model){
+function deleteVm(model) {
 	model.destroy();
 }
 
+// Seach for a virtual machine, using a criteria into the request.
+function searchVirtualMachine(criteria) {
+	promiseVirtualMachines.reset(null, {silet: true});
+	if(criteria === undefined){
+		return promiseVirtualMachines.fetch();
+	} return  promiseVirtualMachines.fetch({data: criteria});
+}
+
+
 module.exports = {
 	save: saveVirtualMachine,
-	deleteVm: deleteVm
+	deleteVm: deleteVm,
+	search: searchVirtualMachine
 };
