511589b1f63377593a52f9eb9458e317bdf94a71
Pierre <pierre.besson7@gmail.com>
2014-02-16 00:45:46 +0100

    Add the view of the virtualMachine to be able to treat the promise delete.

--- app/views/virtualMachine-view.js
+++ app/views/virtualMachine-view.js
@@ -1,8 +1,7 @@
-/*global Backbone, _, $*/
+/*global Backbone, _ */
 var template = require('./templates/virtualMachine');
-var form_helper = require('../lib/form_helper');
 var VmSvc = require('../service/ServiceVirtualMachine');
-
+var ErrorHelper = require('../lib/error_helper');
 module.exports = Backbone.View.extend({
 	tagName: 'div',
 	template: template,
@@ -15,11 +14,10 @@ module.exports = Backbone.View.extend({
 		//this.model.on('sync', this.treatServerResponse, this);
 		this.listenTo(this.model, 'validated:valid', this.modelValid);
 		this.listenTo(this.model, 'validated:invalid', this.modelInValid);
-		this.listenTo(this.model,'destroy',this.deleteSuccess);
+		this.listenTo(this.model, 'destroy', this.deleteSuccess);
 		if (this.model.has('id')) {
 			this.model.fetch();
 		}
-		
 		this.osList = [{
 			id: "1",
 			label: "Windows 8"
@@ -36,19 +34,29 @@ module.exports = Backbone.View.extend({
 		Backbone.history.navigate("updateVirtualMachine/" + this.model.get('id'), true);
 	},
 
-	delete: function deleteVm(event){
+	delete: function deleteVm(event) {
 		event.preventDefault();
+		var model = this;
 		//call suppression service
-		VmSvc.deleteVm(this.model);
+		VmSvc.deleteVm(this.model)
+			.then(function(success) {
+				model.deleteSuccess(success);
+			}, function error(argument) {
+				model.deleteError(argument);
+			});
 	},
-
-	deleteSuccess: function deleteSuccess(event){
+	deleteSuccess: function deleteVmSuccess(response) {
 		//remove the view from the DOM
 		this.remove();
 		//navigate to next page
-		Backbone.history.navigate("/",true);
+		Backbone.history.navigate("/", true);
 	},
-
+	deleteError: function errorVmDelete(errorResponse) {
+		console.log('Error', errorResponse);
+		ErrorHelper.manageResponseErrors(errorResponse, {
+			isDisplay: true
+		});
+	},
 	render: function renderVirtualMachine() {
 		var jsonModel = this.model.toJSON();
 		_.extend(jsonModel, {
