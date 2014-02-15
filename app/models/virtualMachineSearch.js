1896e0afebba48e6f46be4921c834febace42306
Pierre <pierre.besson7@gmail.com>
2014-02-15 21:16:05 +0100

    Add the VirtualMachione model comment.

--- app/models/virtualMachineSearch.js
+++ app/models/virtualMachineSearch.js
@@ -1,8 +1,12 @@
 var VirtualMachine = require('./virtualMachine');
 
-//define a criteria for a virtual machines search
-//inherit of all property of a virtual machine
+/*
+  Define a criteria for a virtual machines search
+  inherit of all property of a virtual machine with
+  the extend method which is inherit by the Backbone.Model
+*/
 module.exports = VirtualMachine.extend({
-	//Override the application validation, in order to not block the search.
+	//Override the application validation.
+	//If we have a required criteria in the search object it would be defined here.
 	validation: {}
 });
