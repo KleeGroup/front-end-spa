52390ffad8c6cda365f105c366c2f94580bbc1a2
Pierre <pierre.besson7@gmail.com>
2014-02-15 23:33:01 +0100

    improve the error helper in order to be able to display http errors, correct javascript mistakes of the first version, and add a isDisplay option and a displayError method.

--- app/lib/error_helper.js
+++ app/lib/error_helper.js
@@ -1,26 +1,55 @@
+/*global _*/
+var BackboneNotification = require('./backbone_notification');
 // transform errors send by API to application errors.
-function manageResponseErrors(response){
+function manageResponseErrors(response, options) {
+	options = options || {};
 	var responseErrors = response.responseJSON;
-	if(responseErrors != undefined && responseErrors.errors != undefined){
-		// there errors in the response
-		var globalMessages = []
-		var fieldErrors = {}
-		_.each(responseErrors.errors, function(error){
-			if(error.fieldName != undefined && error.fieldName.length > 0){
-				fieldErrors[error.fieldName] = error.message
-			}else{
-				globalMessages.push(error.message)
-			}
-		});
-		return {
-			fieldErrors: fieldErrors,
-			globalErrors: globalMessages
+	/**/
+	var globalErrors = [];
+	var fieldErrors = {};
+	if (responseErrors !== undefined && responseErrors !== null) {
+		/*Case of an HTTP Error (as an example 404).*/
+		if (responseErrors.error !== undefined && responseErrors !== null) {
+			//The response json should have the following structure : {statusCode: 404, error: "Not Found"}
+			globalErrors.push({
+				type: "error",
+				message: '' + responseErrors.statusCode + ' ' + responseErrors.error,
+				creationDate: Date.now()
+			});
+		} else if (responseErrors.errors !== undefined) {
+			// there errors in the response
+			_.each(responseErrors.errors, function(error) {
+				if (error.fieldName !== undefined && error.fieldName.length > 0) {
+					fieldErrors[error.fieldName] = error.message;
+				} else {
+					globalErrors.push(error.message);
+				}
+			});
 		}
 	}
-	//no errors in response
-	return null;
+	//If there is no errors, do nothing.
+	if ((_.isEmpty(fieldErrors) && _.isEmpty(globalErrors))) {
+		return null;
+	} else {
+		var errors = {
+			fieldErrors: fieldErrors,
+			globalErrors: globalErrors
+		};
+		//If the display options is passed in argument, we display the options.
+		if(options.isDisplay){
+			displayErrors(errors);
+		}
+		return errors;
+	}
 }
 
-module.exports = {
-	manageResponseErrors: manageResponseErrors
+
+function displayErrors(errors) {
+	if (errors !== undefined && errors.globalErrors !== undefined) {
+		BackboneNotification.addNotification(errors.globalErrors, true);
+	}
 }
+module.exports = {
+	manageResponseErrors: manageResponseErrors,
+	display: displayErrors
+};
