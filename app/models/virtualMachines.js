8341dad7f0ca21d296253c8668f99d9c27b9f175
Pierre <pierre.besson7@gmail.com>
2014-02-15 21:20:23 +0100

    Add the comments on the Virtual machine collection.

--- app/models/virtualMachines.js
+++ app/models/virtualMachines.js
@@ -1,9 +1,13 @@
 /*global Backbone*/
+//Dependencies.
 var URL = require('./URL');
 var VirtualMachine = require('./virtualMachine');
 
 //define a collection of virtual machines
 module.exports = Backbone.Collection.extend({
+	//A collection only need a model property in order to _Type_ each element of the collection.
 	model: VirtualMachine,
+	//The url will  be use in order to do all the [CRUD](http://en.wikipedia.org/wiki/Create,_read,_update_and_delete)
+	// operations on the model with the REST api. All operations will only exchange json between client and server.s
 	url: URL.virtualMachine
 });
