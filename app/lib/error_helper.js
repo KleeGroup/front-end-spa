ba7f0b3078b3317fd407e10d026014e8677ca92d
Pierre <pierre.besson7@gmail.com>
2014-02-16 16:10:57 +0100

    Update the error helper to have nice display default options, it can be done without it but it is simplier to read then. Also correct the HTTP error handle in oder to not have backbone notification structure by default, it is decieded on display.

--- app/lib/error_helper.js
+++ app/lib/error_helper.js
@@ -11,11 +11,7 @@ function manageResponseErrors(response, options) {
 		/*Case of an HTTP Error (as an example 404).*/
 		if (responseErrors.error !== undefined && responseErrors !== null) {
 			//The response json should have the following structure : {statusCode: 404, error: "Not Found"}
-			globalErrors.push({
-				type: "error",
-				message: '' + responseErrors.statusCode + ' ' + responseErrors.error,
-				creationDate: Date.now()
-			});
+			globalErrors.push( '' + responseErrors.statusCode + ' ' + responseErrors.error);
 		} else if (responseErrors.errors !== undefined) {
 			// there errors in the response
 			_.each(responseErrors.errors, function(error) {
@@ -36,20 +32,41 @@ function manageResponseErrors(response, options) {
 			globalErrors: globalErrors
 		};
 		//If the display options is passed in argument, we display the options.
-		if(options.isDisplay){
+		if (options.isDisplay || options.model) {
 			displayErrors(errors);
+		}
+		if (options.model) {
+			setModelErrors(options.model, errors);
 		}
 		return errors;
 	}
 }
 
-
+//Display errors which are defined into the errors.global
 function displayErrors(errors) {
 	if (errors !== undefined && errors.globalErrors !== undefined) {
-		BackboneNotification.addNotification(errors.globalErrors, true);
+		var errorsGlobal = [];
+		errors.globalErrors.forEach(function convertErrorsIntoNotification(element) {
+			errorsGlobal.push({
+				type: "error",
+				message: element,
+				creationDate: Date.now()
+			});
+		});
+		BackboneNotification.addNotification(errorsGlobal, true);
+	}
+}
+
+//Set the *model* errors in the fieldErrors.
+function setModelErrors(model, errors) {
+	if (errors !== undefined && errors.fieldErrors !== undefined) {
+		model.set({
+			'errors': errors.fieldErrors
+		});
 	}
 }
 module.exports = {
 	manageResponseErrors: manageResponseErrors,
-	display: displayErrors
+	display: displayErrors,
+	setModelErrors: setModelErrors
 };
