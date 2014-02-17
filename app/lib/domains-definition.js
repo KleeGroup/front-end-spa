3e25a1598af2965d5867336299eec633272fae4d
Pierre Besson <pierre.besson7@gmail.com>
2014-02-17 09:18:07 +0100

    Add the domains definitions and domains.

---
+++ app/lib/domains-definition.js
@@ -0,0 +1,39 @@
+module.exports = {
+	"virtualMachine": {
+		"name": {
+			"domain": "DO_TEXTE_50",
+			"required": true
+		},
+		"nbCpu": {
+			"domain": "DO_ENTIER",
+			"required": true
+		},
+		"osId": {
+			"domain": "DO_ID",
+			"required": true
+		},
+		"memory": {
+			"domain": "DO_ENTIER",
+			"required": true
+		},
+		"diskCapacity": {
+			"domain": "DO_ENTIER",
+			"required": true
+		},
+		"users": {
+			"domain": "DO_LIST",
+			"required": true
+		},
+		"startDate": {
+			"domain": "DO_DATE",
+			"required": true
+		},
+		"endDate": {
+			"domain": "DO_DATE"
+		},
+		"os": {
+			"domain": "DO_TEXTE_50",
+			"required": true
+		}
+	}
+};
---
+++ app/lib/domains.js
@@ -0,0 +1,38 @@
+module.exports = {
+	"DO_ENTIER": {
+		"type": "number",
+		"validation": [{
+			"type": "regex",
+			"value": "^\\d+$"
+		}],
+	},
+	"DO_DATE": {
+		"type": "date",
+		"validation": [{
+			"type": "name",
+			"value": "date"
+		}]
+	},
+	"DO_TEXTE_50": {
+		"type": "number",
+		"validation": [{
+			"type": "regex",
+			"value": "^\\+$"
+		}]
+	},
+	"DO_LISTE": {
+		"type": "number",
+		"validation": [{
+			"type": "regex",
+			"value": "^\\+$"
+		}]
+	},
+	"DO_ID": {
+		"type": "text",
+		"validation": [{
+			"type": "regex",
+			"value": "^\\+$"
+		}]
+	}
+
+};
