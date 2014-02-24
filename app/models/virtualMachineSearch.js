6cc5be90c258a480aaf492f4150ac3af8517677c
Olivier Boitel <olivier.boitel@kleegroup.com>
2014-02-24 17:35:34 +0100

    correction meta data a valider

--- app/models/virtualMachineSearch.js
+++ app/models/virtualMachineSearch.js
@@ -5,8 +5,23 @@ var VirtualMachine = require('./virtualMachine');
   inherit of all property of a virtual machine with
   the extend method which is inherit by the Backbone.Model
 */
+var domDef = require('../lib/domains-definition');
+
 module.exports = VirtualMachine.extend({
 	//Override the application validation.
 	//If we have a required criteria in the search object it would be defined here.
-	validation: {}
+	validation: {},
+
+	metadatas: {	
+		startDate: {
+			metadata: domDef.virtualMachine.startDate,
+			required: false
+		},
+		// Override with an other entity domain
+		name: {
+			metadata: domDef.virtualMachineSearch.name,
+			domain: "DO_TEXTE_50",
+			label: "virtualMachine.name"
+		},
+	},
 });
